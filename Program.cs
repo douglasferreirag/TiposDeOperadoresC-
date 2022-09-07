08b485ceeb3de356a7277ab7a7a77bad0cc0a83d
Douglas <douglas.ferreirag@gmail.com>
2022-09-06 21:59:45 -0300

    TryParse

--- Program.cs
+++ Program.cs
@@ -1,9 +1,20 @@
 ﻿using ExemploFundamentos.Models;
 
 
-double a = 4/ (2 + 2);
+string a = "15-";
 
-Console.WriteLine(a);
+//int b =0;
+
+int.TryParse(a, out int b);
+
+Console.WriteLine(b);
+
+Console.WriteLine("Conversão realizada com sucesso");
+
+
+// double a = 4/ (2 + 2);
+
+// Console.WriteLine(a);
 
 
 // int a = 5;
